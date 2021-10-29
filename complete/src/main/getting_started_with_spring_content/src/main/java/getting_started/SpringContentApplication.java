58994be847c6c672083dd98124105ae2b2d648ba
Chris <chrisdel@protonmail.com>
2021-10-29 17:33:41 -0400

    part one of the walk through is complete

---
+++ complete/src/main/getting_started_with_spring_content/src/main/java/getting_started/SpringContentApplication.java
@@ -0,0 +1,12 @@
+package getting_started;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringContentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringContentApplication.class, args);
+	}
+}
